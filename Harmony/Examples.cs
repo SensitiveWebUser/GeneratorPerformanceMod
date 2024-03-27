13a0a3cd49d7a275d7802c5fac7e5dea46ccb90f
sensitivewebuser <55949728+SensitiveWebUser@users.noreply.github.com>
2024-03-27 02:11:02 +0000

    Add project files.

---
+++ Harmony/Examples.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MyTestMod.Harmony
+{
+    [HarmonyPatch(typeof(XUiC_MainMenu))]
+    public class MyTestModPatch
+    {
+        public static void Postfix(XUiC_MainMenu __instance)
+        {
+            if (__instance.GetChildById("btnContinueGame") is XUiC_SimpleButton btnContinueGame)
+            {
+                btnContinueGame.Label = "Continue ASA Game";
+                btnContinueGame.OnPressed += (_sender, _mouseButton) =>
+                {
+                    Log.Out("btnNewGame pressed");
+                };
+            }
+        }
+    }
+}
